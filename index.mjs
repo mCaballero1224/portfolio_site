d3d0eb820e3bfbd1e4bbb963050e9b8aa7fe61f5
Michael Caballero <mcaballero.dev@gmail.com>
2025-02-22 06:27:45 -0800

    Remove root get route

--- index.mjs
+++ index.mjs
@@ -44,9 +44,11 @@ app.use((error, req, res, next) => {
 app.use('/db', dbRouter);
 
 // Home Page
+/*
 app.get('/', (req, res) => {
     res.render('index');
 });
+*/
 
 app.get('/home', (req, res) => {
     res.render('index');
