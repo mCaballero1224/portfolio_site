adfc854af63d763b928d11aebecb50810461e74b
Michael Caballero <mcaballero.dev@gmail.com>
2025-02-27 08:40:34 -0800

    Refactor project rendering

--- public/javascripts/projects.js
+++ public/javascripts/projects.js
@@ -2,48 +2,84 @@
  * Handles SQL query for my projects 
  */
 
+
+const createTitle = (title, link) => {
+  /* Create new elements */
+  let titleElement = document.createElement('li');
+  let linkElement = document.createElement('a');
+  /* Add link to title element as a child */
+  titleElement.appendChild(linkElement);
+  /* Add attributes to title element*/
+  titleElement.classList.add('project-title');
+  titleElement.id = `${title}-project`
+  /* Add attributes to link element*/
+  linkElement.href = link
+  linkElement.target = "_blank"; /* Opens new window */
+  linkElement.textContent = title;
+
+  return titleElement;
+}
+
+const createDescription = (description) => {
+  let descriptionElement = document.createElement('li');
+  descriptionElement.classList.add('project-description');
+  descriptionElement.textContent = description;
+
+  return descriptionElement;
+}
+
+const createImageLink = (imageUrl, link) => {
+  /* Create elements */
+  let imageElement = document.createElement('img');
+  let linkElement = document.createElement('a');
+  /* Add classes to elements */
+  imageElement.classList.add('project-image');
+  linkElement.classList.add('image-link');
+  /* Append link to image element as a child */
+  imageElement.appendChild(linkElement);
+  /* Add attributes */
+  imageElement.src = imageUrl;
+  linkElement.href = link;
+  linkElement.target = "_blank";
+
+  return imageElement;
+}
+
+const createOverview = (title, link, description) => {
+  let overview = document.createElement('ul');
+  overview.classList.add('project-overview');
+  overview.appendChild(createTitle(title, link));
+  overview.appendChild(createDescription(description));
+  return overview;
+}
+
 const fetchprojects = async () => {
   try {
-    // Get project data
+    /* Get project data */
     const response = await fetch('/db/projects');
     if (!response.ok) {
       throw new Error('Failed to fetch data');
     }
     const projectsData = await response.json();
-    // Set up HTML list
+    /* Set up HTML list */
     const projectList = document.getElementById('project-list');
     projectsData.forEach(project => {
-      // Create list element and append to list
+      console.log(project);
+      /* Create list element and append to list */
       const li = document.createElement('li');
+      /* Add element class */
       li.classList.add('project-item');
-      const projectOverview = document.createElement('ul');
-      projectOverview.classList.add('project-overview');
+      /* Create image link element and append */
+      const imageLink = createImageLink(project.project_image, 
+        project.project_link);
+      li.appendChild(imageLink);
+      /* Create overview element and append */
+      const overview = createOverview(project.project_name,
+        project.project_link, project.project_description);
+      li.appendChild(overview);
+      /* Append list item to list */
       projectList.appendChild(li);
-      // Set up content of list element
-      titleElement = document.createElement('li');
-      titleElement.classList.add('project-title');
-      projectImageElement = document.createElement('img');
-      projectImageElement.classList.add('project-image');
-      projectDescriptionElement = document.createElement('li');
-      projectDescriptionElement.classList.add('project-description');
-      projectLink = document.createElement('a');
-      projectLink.appendChild(projectImageElement);
-      projectLink.classList.add('project-link');
 
-      // Add data to spans
-      titleElement.textContent = project.project_name;
-      projectLink.href = project.project_link;
-      projectLink.target = "_blank";
-      projectDescriptionElement.textContent = project.project_description;
-      projectImageElement.src = project.project_image;
-      projectImageElement.alt = `Image representing ${project_name}; also a link to its GitHub repo`
-
-      // Append spans to list item
-      li.id = project.project_name;
-      li.appendChild(projectLink);
-      li.appendChild(projectOverview);
-      projectOverview.appendChild(titleElement);
-      projectOverview.appendChild(projectDescriptionElement);
     });
   } catch (error) {
     console.error('Error fetching data:', error);
