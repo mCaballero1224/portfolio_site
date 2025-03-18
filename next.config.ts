fcc568aa904f1a8f0aebe3a2288268a1c551f12f
Michael Caballero <mcaballero.dev@gmail.com>
2025-03-17 23:15:28 -0700

    Redirect root path to '/home'

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
