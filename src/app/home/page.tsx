d590a138257565ee76e67972aff1da98612e4a05
Michael Caballero <mcaballero.dev@gmail.com>
2025-03-19 00:00:26 -0700

    Create site pages

---
+++ src/app/home/page.tsx
@@ -0,0 +1,9 @@
+import HeroSection from '@/components/HeroSection';
+
+export default function HomePage() {
+  return (
+    <main className="bg-gradient-to-b from-green-100 to-teal-50 flex flex-col h-screen w-screen">
+      <HeroSection />
+    </main>
+  );
+}
